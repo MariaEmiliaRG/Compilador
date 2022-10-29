b21d74f49f86051f9b844b9edf6bce62688fb95e
Bryan Vargas <54085468+bryanv07@users.noreply.github.com>
2022-10-29 10:23:28 -0500

    Mejora a generalización de listas
    Se realizaron cambios para hacer la biblioteca más general.

--- list.c
+++ list.c
@@ -4,27 +4,26 @@
 #include <stdlib.h>
 
 int size = 0;
-int counter = 0;
 
 struct node{
     char str[20];
     int token;           
     struct node *next; 
-}*head, *tail;
+};
 
-void createList(char const *name){
+void createList(char const *name, struct node *head, struct node *tail){
     head = (struct node *)malloc(sizeof(struct node)); 
     if(head == NULL){
         printf("ERROR with memory");
         exit(0);
     }
     strcpy(head -> str, name);
-    head -> token = counter++;
+    head -> token = 0;
     head -> next = NULL;
     tail = head;
 }
 
-void addElement(char const *name){
+void addElement(char const *name, struct node *tail){
     struct node *temp;
     temp = (struct node *)malloc(sizeof(struct node));
     if(temp == NULL){
@@ -32,13 +31,13 @@ void addElement(char const *name){
         exit(0);
     }
     strcpy(temp -> str, name);
-    temp -> token = counter++;
+    temp -> token = tail -> token + 1;
     temp -> next = NULL;
     tail -> next = temp;
     tail = tail->next;
 }
 
-void traverse(){
+void traverse(struct node *head){
     struct node *temp;
     temp = (struct node *)malloc(sizeof(struct node));
     if(head == NULL){
